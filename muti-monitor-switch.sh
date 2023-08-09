df05f0d633f6914175b7f9ca8e48130e603872c6
liukairui <tclkr@live.com>
2023-08-10 02:38:55 +1200

    feat: muti monitor

---
+++ muti-monitor-switch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+external_output=$(swaymsg -t get_outputs | jq -r '.[] | select(.name | startswith("DP-") or startswith("HDMI-")) | .name')
+echo fuck
+echo $external_output
+
+if [ -n "$external_output" ]; then
+  case "$1" in
+    "external_only")
+      swaymsg "output eDP-1 disable" \
+             && swaymsg "output $external_output enable" \
+             && swaymsg "output $external_output resolution 1920x1080 position 0 0"
+      ;;
+    "edp_only")
+      swaymsg "output eDP-1 enable" \
+             && swaymsg "output eDP-1 resolution 2880x1800 position 0 0 scale 2" \
+             && swaymsg "output $external_output disable"
+      ;;
+    "both")
+      swaymsg "output eDP-1 enable" \
+             && swaymsg "output eDP-1 resolution 2880x1800 position 1920 682 scale 2" \
+             && swaymsg "output $external_output enable primary" \
+             && swaymsg "output $external_output resolution 1920x1080 position 0 0"
+      ;;
+    *)
+      echo "Unknown option: $1"
+      ;;
+  esac
+fi
--- run-chrome.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source ~/.dotfile/env.sh
-/usr/bin/google-chrome-beta --gtk-version=4

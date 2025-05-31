18dc20d4e83f88b92b9694c4eb105fb7ee57e48e
LiuKairui <tclkr@live.com>
2025-05-31 14:13:10 +0800

    fix: cfw

--- shell/env.sh
+++ shell/env.sh
@@ -9,8 +9,8 @@ export LANG=zh_CN.UTF-8
 export LANGUAGE=zh_CN:en_US
 export LC_MONETARY=zh_CN.UTF-8
 export LC_TIME=zh_CN.UTF-8
-export GTK_IM_MODULE=fcitx
-export QT_IM_MODULE=fcitx
+# export GTK_IM_MODULE=fcitx
+export QT_IM_MODULE="wayland;fcitx;ibus"
 export GLFW_IM_MODULE=fcitx
 export INPUT_METHOD=fcitx
 export XMODIFIERS=@im=fcitx

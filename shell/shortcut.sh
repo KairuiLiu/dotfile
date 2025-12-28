4d2270b9602bd084d343377005ea1f3591d04a8c
LiuKairui <liukairui.855@bytedance.com>
2025-12-29 01:33:23 +0800

    feat: 更好的 shell 体验

--- shell/shortcut.sh
+++ shell/shortcut.sh
@@ -2,23 +2,41 @@ alias weatherHD="curl zh.wttr.in/Beijing%20Haidian"
 alias rr="/opt/homebrew/bin/im-select com.apple.keylayout.ABC ; ranger"
 alias fzfh="FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob \"!.git/*\" --glob \"!.node_modules/*\"' fzf"
 alias cp="cp -i"
+alias ex="extract"
 alias df='df -h'
-alias free='free -m'
-alias np='nano -w PKGBUILD'
 alias python='python3'
 alias chrome='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
 alias chrome-dev='/Applications/Google\ Chrome\ Dev.app/Contents/MacOS/Google\ Chrome\ Dev'
 alias firefox='/Applications/Firefox.app/Contents/MacOS/firefox'
 alias vlc='/Applications/VLC.app/Contents/MacOS/VLC'
 alias krita='/Applications/krita.app/Contents/MacOS/krita'
-alias more=less
-alias _cat='cat'
-alias cat='ccat'
-alias _top='top'
-alias top='htop'
+alias more='less'
 alias pnpx='pnpm --dlx '
 alias k='~/code/k/k.bash'
 alias pdm_cli='~/Public/PDM/pdm_cli'
+alias mkdir='mkdir -p'
+if [[ -o interactive ]]; then
+    alias _ls='command ls'
+    alias _ll='command ls -alh'
+    alias ls='eza --color=auto'
+    alias ll='eza -al --git --color=auto'
+    alias _tree='command tree'
+    alias tree='eza --tree'
+    alias _grep='command grep'
+    alias grep='rg'
+    alias _find='command find'
+    alias find='fd'
+    alias _top='command top'
+    alias top='btop'
+    alias _du='command du -sh'
+    alias du='dust'
+    alias _diff='command diff'
+    alias diff='delta'
+    alias _ps='command ps'
+    alias ps='procs'
+    alias _ping='command ping'
+    alias ping='gping'
+fi
 
 source ~/.dotfile/keep_local/source.sh
 
@@ -32,29 +50,8 @@ function ross() {
 }
 
 afplay() {
-    [[ "$1" == "-v" ]] && shift && shift
-    command afplay -v 0.1 "$@"
-}
-
-ex() {
-    if [ -f $1 ]; then
-        case $1 in
-        *.tar.bz2) tar xjf $1 ;;
-        *.tar.gz) tar xzf $1 ;;
-        *.bz2) bunzip2 $1 ;;
-        *.rar) unrar x $1 ;;
-        *.gz) gunzip $1 ;;
-        *.tar) tar xf $1 ;;
-        *.tbz2) tar xjf $1 ;;
-        *.tgz) tar xzf $1 ;;
-        *.zip) unzip $1 ;;
-        *.Z) uncompress $1 ;;
-        *.7z) 7z x $1 ;;
-        *) echo "'$1' cannot be extracted via ex()" ;;
-        esac
-    else
-        echo "'$1' is not a valid file"
-    fi
+    local args=("$@")
+    command afplay -v 0.1 "${args[@]/#-v*/}"
 }
 
 function y() {
